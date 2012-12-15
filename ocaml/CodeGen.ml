f50fef962002ee91d06c13fa081004c9631e7314
qiqi <mz2369@columbia.edu>
2012-12-15 09:11:43 -0500

    compiler

--- ocaml/CodeGen.ml
+++ ocaml/CodeGen.ml
@@ -2,9 +2,9 @@
 ellipse b;
 def line function(line a,line b)
 {
- if(a//b)
-  return a//b;
-else 
+if(a//b)
+return a//b;
+else
 return a+b;
 }
 *)
@@ -27,153 +27,150 @@ let gen_type=function
 |STRING->"string"
 
 
-(*test case:gen_expr ( Binop(PointEx(Point(INT(1),INT(2))),EE,PointEx(Point(INT(1),INT(2))))  );;*)
-(* gen_expr ( Binop(PointEx(Point(INT(1),INT(2))),PARA,PointEx(Point(INT(1),INT(2))))  );;*)
+(*test case:gen_expr ( Binop(PointEx(Point(INT(1),INT(2))),EE,PointEx(Point(INT(1),INT(2)))) );;*)
+(* gen_expr ( Binop(PointEx(Point(INT(1),INT(2))),PARA,PointEx(Point(INT(1),INT(2)))) );;*)
 (* gen_actual_list [PointEx(Point(INT(1),INT(2)));PointEx(Point(INT(1),INT(2)))];;*)
 (* gen_expr (Call("print", [PointEx(Point(INT(1),INT(2)));PointEx(Point(INT(1),INT(2)))]));;*)
-let rec	 gen_expr expr=
-	match expr with
-	| NUM(f) ->string_of_float f
-	| INT(i)-> string_of_int i
-	| ID(id)-> id
-	| Binop( expr1,op ,expr2)->
-		begin
-			match op with 
-			|EE->"("^(gen_expr expr1)^")==("^(gen_expr expr2)^")"
-		  |NE->"("^(gen_expr expr1)^")!=("^(gen_expr expr2)^")"
-			|SE->"("^(gen_expr expr1)^")<=("^(gen_expr expr2)^")"
-			|LE->"("^(gen_expr expr1)^")>=("^(gen_expr expr2)^")"
-			|S->"("^(gen_expr expr1)^")<("^(gen_expr expr2)^")"
-			|L->"("^(gen_expr expr1)^")>("^(gen_expr expr2)^")"
-		  |PLUS->"("^(gen_expr expr1)^")+("^(gen_expr expr2)^")"
-			|MINUS->"("^(gen_expr expr1)^")-("^(gen_expr expr2)^")"
-			|MUL->"("^(gen_expr expr1)^")*("^(gen_expr expr2)^")"
-			|DIV->"("^(gen_expr expr1)^")/("^(gen_expr expr2)^")"
-			|PERC ->"("^(gen_expr expr1)^")%("^(gen_expr expr2)^")"
-			|PARA->"(isParallel(*"^gen_expr expr1^",*"^gen_expr expr2^"))"
-			|INTERS->"(getIntersect(*"^gen_expr expr1^",*"^gen_expr expr2^"))"
-			|RELAT->"(getRelation("^gen_expr expr1^","^gen_expr expr2^"))"
-			|TE->"(isCongruent("^gen_expr expr1^","^gen_expr expr2^"))"
-			|SS->"(isPerimeterLessThan("^gen_expr expr1^","^gen_expr expr2^"))"
-			|LL->"(isPerimeterLargerThan("^gen_expr expr1^","^gen_expr expr2^"))"
-			|SSE->"(isPerimeterLessOrEqual("^gen_expr expr1^","^gen_expr expr2^"))"
-			|LLE->"(isPerimeterLargerOrEqual("^gen_expr expr1^","^gen_expr expr2^"))"
-			end
-	| Assign(id,expr)->id^"="^(gen_expr expr)
+let rec	gen_expr expr=
+match expr with
+| NUM(f) ->string_of_float f
+| INT(i)-> string_of_int i
+| ID(id)-> id
+| Binop( expr1,op ,expr2)->
+begin
+match op with
+|EE->"("^(gen_expr expr1)^")==("^(gen_expr expr2)^")"
+|NE->"("^(gen_expr expr1)^")!=("^(gen_expr expr2)^")"
+|SE->"("^(gen_expr expr1)^")<=("^(gen_expr expr2)^")"
+|LE->"("^(gen_expr expr1)^")>=("^(gen_expr expr2)^")"
+|S->"("^(gen_expr expr1)^")<("^(gen_expr expr2)^")"
+|L->"("^(gen_expr expr1)^")>("^(gen_expr expr2)^")"
+|PLUS->"("^(gen_expr expr1)^")+("^(gen_expr expr2)^")"
+|MINUS->"("^(gen_expr expr1)^")-("^(gen_expr expr2)^")"
+|MUL->"("^(gen_expr expr1)^")*("^(gen_expr expr2)^")"
+|DIV->"("^(gen_expr expr1)^")/("^(gen_expr expr2)^")"
+|PERC ->"("^(gen_expr expr1)^")%("^(gen_expr expr2)^")"
+|PARA->"(isParallel(*"^gen_expr expr1^",*"^gen_expr expr2^"))"
+|INTERS->"(getIntersect(*"^gen_expr expr1^",*"^gen_expr expr2^"))"
+|RELAT->"(getRelation(*"^gen_expr expr1^",*"^gen_expr expr2^"))"
+|TE->"(isCongruent("^gen_expr expr1^","^gen_expr expr2^"))"
+|SS->"(isPerimeterLessThan("^gen_expr expr1^","^gen_expr expr2^"))"
+|LL->"(isPerimeterLargerThan("^gen_expr expr1^","^gen_expr expr2^"))"
+|SSE->"(isPerimeterLessOrEqual("^gen_expr expr1^","^gen_expr expr2^"))"
+|LLE->"(isPerimeterLargerOrEqual("^gen_expr expr1^","^gen_expr expr2^"))"
+end
+| Assign(id,expr)->id^"="^(gen_expr expr)
   | Call(funcname, actual_list)->
-		begin
-			match funcname with 
-			|_ ->funcname^"("^(gen_actual_list actual_list)^")"
-			end
-	| String(str)->str
-	| PointEx(Point(expr1,expr2))->"new point((float) "^(gen_expr expr1)^",(float) "^(gen_expr expr2)^")"
-	| LineEx(Line(point1,point2))->"new line("^(gen_point point1)^","^(gen_point point2)^")"
-	| PolygonEx(Polygon(point_list))->"new polygon("^(string_of_int (List.length point_list))^",new point*["^(string_of_int (List.length point_list))^"]"^(gen_points point_list)
-	| EllipseEx(Ellipse(point, expr1,expr2))->"new ellipse("^(gen_point point)^",(float) "^(gen_expr expr1)^",(float) "^(gen_expr expr2)^")"
+begin
+match funcname with
+|_ ->funcname^"("^(gen_actual_list actual_list)^")"
+end
+| String(str)->str
+| PointEx(Point(expr1,expr2))->"new point((float) "^(gen_expr expr1)^",(float) "^(gen_expr expr2)^")"
+| LineEx(Line(point1,point2))->"new line("^(gen_point point1)^","^(gen_point point2)^")"
+| PolygonEx(Polygon(point_list))->"new polygon("^(string_of_int (List.length point_list))^",new point*["^(string_of_int (List.length point_list))^"]"^(gen_points point_list)
+| EllipseEx(Ellipse(point, expr1,expr2))->"new ellipse("^(gen_point point)^",(float) "^(gen_expr expr1)^",(float) "^(gen_expr expr2)^")"
 
 and gen_actual_list =function
-	actual_list ->"("^(String.concat "," (List.map gen_expr actual_list))^")"
+actual_list ->"("^(String.concat "," (List.map gen_expr actual_list))^")"
 
 
 and gen_point=function
-	|Point(expr1,expr2)->"new point((float) "^(gen_expr expr1)^",(float) "^(gen_expr expr2)^")"
+|Point(expr1,expr2)->"new point((float) "^(gen_expr expr1)^",(float) "^(gen_expr expr2)^")"
 
 and gen_points point_list=
-	"{"^(String.concat "," (List.map gen_point point_list))^"}"
+"{"^(String.concat "," (List.map gen_point point_list))^"}"
 
-	
-	
-	
+
+
+
 (*and stmt=
-	| ExStmt of expr
-	| Return of expr
-	| Block of stmt list
-	| If of expr * stmt * stmt
-	| For of expr * expr * expr * stmt
-	| While of expr * stmt
+| ExStmt of expr
+| Return of expr
+| Block of stmt list
+| If of expr * stmt * stmt
+| For of expr * expr * expr * stmt
+| While of expr * stmt
 *)
 let rec gen_stmt stmt=
-	match stmt with
-	| ExStmt(expr)->(gen_expr expr)^";"
-	| Return(expr)->"return "^(gen_expr expr)^";"
-	| Block(stmt_list)->"{"^(String.concat "\n" (List.map gen_stmt stmt_list))^"}"
-	| If(expr,stmt1,stmt2)->"if("^(gen_expr expr)^")\n"^(gen_stmt stmt1)^"\nelse\n"^(gen_stmt stmt2)
-	| For(expr1,expr2,expr3,stmt)->"for("^(gen_expr expr1)^";"^(gen_expr expr2)^";"^(gen_expr expr3)^")\n"^(gen_stmt stmt)
-	| While(expr,stmt)->"while("^(gen_expr expr)^")\n"^(gen_stmt stmt)
-	
-	
+match stmt with
+| ExStmt(expr)->(gen_expr expr)^";"
+| Return(expr)->"return "^(gen_expr expr)^";"
+| Block(stmt_list)->"{"^(String.concat "\n" (List.map gen_stmt stmt_list))^"}"
+| If(expr,stmt1,stmt2)->"if("^(gen_expr expr)^")\n"^(gen_stmt stmt1)^"\nelse\n"^(gen_stmt stmt2)
+| For(expr1,expr2,expr3,stmt)->"for("^(gen_expr expr1)^";"^(gen_expr expr2)^";"^(gen_expr expr3)^")\n"^(gen_stmt stmt)
+| While(expr,stmt)->"while("^(gen_expr expr)^")\n"^(gen_stmt stmt)
+
+
 (*let test_stmt=
-	let expr=	PointEx(Point(INT(1),INT(2))) in
-	let exstmt=ExStmt(expr) in
-	let block=Block([exstmt;exstmt]) in
-	print_string (gen_stmt block);;
+let expr= PointEx(Point(INT(1),INT(2))) in
+let exstmt=ExStmt(expr) in
+let block=Block([exstmt;exstmt]) in
+print_string (gen_stmt block);;
 
 let test_stmt=
-	let expr=	PointEx(Point(INT(1),INT(2))) in
-	let exstmt=ExStmt(expr) in
-	let for1=For(expr,expr,expr,exstmt) in
-	print_string (gen_stmt for1);;
+let expr= PointEx(Point(INT(1),INT(2))) in
+let exstmt=ExStmt(expr) in
+let for1=For(expr,expr,expr,exstmt) in
+print_string (gen_stmt for1);;
 
 let test_stmt=
-	let expr=	PointEx(Point(INT(1),INT(2))) in
-	let exstmt=ExStmt(expr) in
-	let while1=While(expr,exstmt) in
-	print_string (gen_stmt while1);;
-	*)
-	
+let expr= PointEx(Point(INT(1),INT(2))) in
+let exstmt=ExStmt(expr) in
+let while1=While(expr,exstmt) in
+print_string (gen_stmt while1);;
+*)
+
 
 (*gen_var_decl (INT_TYPE, "a")*)	
 (*let gen_var_decl_list var_list=
 let gen_var_decl (var_type, id)=
-	(gen_type var_type)^" "^id 
-	in
-	String.concat "," (List.map gen_var_decl var_list)
-	in
-	print_string (gen_var_decl_list [(INT_TYPE ,"a");(FLOAT ,"b")]);;
+(gen_type var_type)^" "^id
+in
+String.concat "," (List.map gen_var_decl var_list)
+in
+print_string (gen_var_decl_list [(INT_TYPE ,"a");(FLOAT ,"b")]);;
 *)
 
 let gen_var_decl_list var_list=
 let gen_var_decl (var_type, id)=
-	(gen_type var_type)^" "^id 
-	in
-	String.concat "," (List.map gen_var_decl var_list)
+(gen_type var_type)^" "^id
+in
+String.concat "," (List.map gen_var_decl var_list)
 
 let gen_locals local_list=
 let gen_var_decl (var_type, id)=
-	(gen_type var_type)^" "^id 
-	in
-	match local_list with
-	| []->""
-	| _->(String.concat ";\n" (List.map gen_var_decl local_list)	)^";"	
-	
+(gen_type var_type)^" "^id
+in
+match local_list with
+| []->""
+| _->(String.concat ";\n" (List.map gen_var_decl local_list)	)^";"	
+
 let gen_fdecl fdecl=
-	let ftype=gen_type (fdecl.ftype) in
-	let fname=fdecl.fname in
-	let formal_list=(gen_var_decl_list fdecl.formal_list) in
-	let locals=gen_locals fdecl.locals in
-	let body=String.concat "\n" (List.map gen_stmt fdecl.body) in
-	ftype^" "^fname^" "^"("^formal_list^")\n{"^locals^"\n"^body^"\n}"
-	
-	
+let ftype=gen_type (fdecl.ftype) in
+let fname=fdecl.fname in
+let formal_list=(gen_var_decl_list fdecl.formal_list) in
+let locals=gen_locals fdecl.locals in
+let body=String.concat "\n" (List.map gen_stmt fdecl.body) in
+ftype^" "^fname^" "^"("^formal_list^")\n{"^locals^"\n"^body^"\n}"
+
+
 (*let test_gen_func=
-	let formals=[(INT_TYPE ,"a");(FLOAT ,"b")] in
-	let locals=[(INT_TYPE ,"a");(FLOAT ,"b")] in
-	let fname="func1" in
-	let ftype=INT_TYPE in
-	let expr=	PointEx(Point(INT(1),INT(2))) in
-	let exstmt=ExStmt(expr) in
-	let body=[exstmt;exstmt]in
-	let fdecl_list=[{ftype=ftype;fname=fname;formal_list=formals;locals=locals;body=body};{ftype=ftype;fname=fname;formal_list=formals;locals=locals;body=body}] in
-	let vars=locals in
-	print_string (gen_program(vars,fdecl_list));;
+let formals=[(INT_TYPE ,"a");(FLOAT ,"b")] in
+let locals=[(INT_TYPE ,"a");(FLOAT ,"b")] in
+let fname="func1" in
+let ftype=INT_TYPE in
+let expr= PointEx(Point(INT(1),INT(2))) in
+let exstmt=ExStmt(expr) in
+let body=[exstmt;exstmt]in
+let fdecl_list=[{ftype=ftype;fname=fname;formal_list=formals;locals=locals;body=body};{ftype=ftype;fname=fname;formal_list=formals;locals=locals;body=body}] in
+let vars=locals in
+print_string (gen_program(vars,fdecl_list));;
 *)
 
 
 let gen_program (var_list, fdecl_list)=
-	let vars=gen_locals var_list in
-	let fdecls= String.concat "\n" (List.map gen_fdecl fdecl_list) in
-	let header="#include \"main.h\"\n#include \"stdio.h\"\n" in
-	header^vars^"\n"^fdecls
-
-
-
+let vars=gen_locals var_list in
+let fdecls= String.concat "\n" (List.map gen_fdecl fdecl_list) in
+let header="#include \"main.h\"\n#include \"stdio.h\"\n" in
+header^vars^"\n"^fdecls
---
+++ ocaml/main.h
@@ -0,0 +1,163 @@
+using namespace std;
+#include "stdio.h"
+#include <iostream>
+#include <math.h>
+#include "string.h"
+#include "stdlib.h"
+
+
+class  point
+{
+public:
+    float x;
+    float y;
+    point(float a,float b);
+};
+
+point::point(float a,float b){x=a;y=b;}
+
+class line
+{
+public:
+    point * p1;
+    point  * p2;
+    line(point *  a,point *  b);
+};
+line::line(point* a,point*  b){p1=a;p2=b;}
+
+struct ellipse
+{
+    point * focusPoint;
+    float a;//the length of major semi-axes
+    float b;//the length of minot semi-axes
+    ellipse(point * p, float x, float y);
+};
+
+ellipse::ellipse(point * p, float x, float y){focusPoint=p;a=x;b=y;}
+
+class polygon
+{
+public:
+    int pointNum;
+    point ** pointCollection;
+    polygon(int pointNum, point ** pointCollection);
+};
+polygon::polygon(int pointNum1, point ** pointCollection1){pointNum=pointNum1;pointCollection=pointCollection1;}
+
+
+/************************The following code are used to judge the relation between two lines*******************/
+void displayPoint(point *p)
+{
+    printf("This is a point!\nIt is:\n%f %f", p->x,p->y);
+    
+}
+point * getPoint(float f1, float f2)
+{
+    point *p=new point(f1,f2);
+    return p;
+}
+
+point *getIntersect(line l1, line l2)
+{
+	bool isParallel(line l1, line l2);
+    point *result = getPoint(0,0);
+    if(isParallel(l1,l2))
+    {
+        printf("these two linse are parallel!\n");
+        return result;// if these two lines are parallel, just return the original point with a warning.
+    }
+    else
+    {
+        float x1 = l1.p1->x, y1 = l1.p1->y, x2 = l1.p2->x,y2 = l1.p2->y;
+        float h1 = l2.p1->x, k1 = l2.p1->y, h2 = l2.p2->x, k2 = l2.p2->y;
+        float temp2 = (k1-k2)/(h1-h2), temp1 = (y1-y2)/(x1-x2);
+        float a = temp1-temp2,b = k1-y1-h1*temp2+x1*temp1;
+        float resultX = b/a;
+        float resultY = temp1*(resultX-x1) +y1;
+        result = getPoint(resultX, resultY);
+        return result;
+    }
+    
+}// get the intersect point of two lines
+
+
+
+bool isParallel(line l1, line l2)
+{
+    float deltaX1 = l1.p1->x-l1.p2->x,deltaY1 = l1.p1->y-l1.p2->y;
+    float deltaX2 = l2.p1->x-l2.p2->x, deltaY2 = l2.p1->y-l2.p2->y;
+    if(deltaX1 == 0 && deltaX2 == 0)
+        return true;
+    if(deltaX1*deltaX2 == 0 && deltaX1+deltaX2!=0)
+        return false;
+    if(deltaY1/deltaX1 != deltaY2/deltaX2)
+        return false;
+    return true;
+}//judge whether two lines are parallel
+float getDis(point *p1, point *p2)
+{
+    return sqrt(pow((p1->x-p2->x),2.0)+pow((p1->y-p2->y),2.0));
+}
+
+char * getRelation(ellipse  circle1, ellipse  circle2)
+{
+	char * str=(char*)malloc(sizeof(char)*30);
+    if(circle1.a!=circle1.b || circle2.a!=circle2.b)
+    {
+        printf("These two shapes are not both circles!\n");
+	str="";
+        return str;//inform a mistake
+    }
+    
+    float r1 = circle1.a, r2 = circle2.a;
+        
+    float dis = getDis(circle1.focusPoint, circle2.focusPoint);
+    if(fabs(dis-r1-r2)<=0.0001)
+        strcpy(str,"circumscribe");// they are circumscribe
+    else if(fabs(dis-fabs(r1-r2))<=0.0001)
+        strcpy(str,"inscribe");// they are inscribe
+    else if(dis<fabs(r1-r2))
+        strcpy(str,"contain");// one circle contains another one
+    else if(dis>r1+r2)
+          strcpy(str,"seperate");//they are seperated
+    else if(r1+r2>dis && dis > fabs(r1-r2))
+       strcpy(str,"secant");// they are secant
+    
+    return str;
+    
+}
+
+
+
+/*
+int main()
+{
+	printf("%f\n", (new point((float)1,(float)2))->x);
+        printf("%f\n", (( new line( new point(1.1,1.2), new point(2.1,2.2)))->p1->x));
+	printf("%f\n", ((new ellipse(new point(1.1,1.2),(float)3,(float)4))->focusPoint->x));
+	//new point[]={new point(1.1,1.2),new point(1.1,1.2),new point(1.1,1.2)}
+	int num=2;
+	//printf("%d\n",(new int[num]{1,2})[0]);
+	//point  p[]={point(1.1,1.2),point(1.1,1.2),point(1.1,1.2)};
+	//new polygon(num, new point[num]{point(1.1,1.2),point(1.1,1.2),point(1.1,1.2)} );
+	//new point*[num]{new point(1.1,1.2), new point(1.1,1.2)};
+	printf("%d\n", (new polygon(3, new point*[3]{new point(1.1,1.2), new point(1.1,1.2), new point(3.1,1.2)}            ))->pointNum);
+}
+*/
+
+/*
+int main()
+{line * l1;
+line * l2;
+bool a;
+l1=new line(new point((float) 1,(float) 1),new point((float) 2,(float) 2));
+l2=new line(new point((float) 3,(float) 3),new point((float) 4,(float) 4));
+a=(isParallel(*l1,*l2));
+}
+
+*/
+
+
+
+
+
